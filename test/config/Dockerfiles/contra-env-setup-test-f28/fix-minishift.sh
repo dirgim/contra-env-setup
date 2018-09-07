1e474895088d6dc2ba5785beadbce441bd1f9f5e
kpavic <kpavic@redhat.com>
2018-09-07 21:44:09 +0200

    Fix fix-minishift.sh script

--- test/config/Dockerfiles/contra-env-setup-test-f28/fix-minishift.sh
+++ test/config/Dockerfiles/contra-env-setup-test-f28/fix-minishift.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-/root/minishift-1.23.0-linux-amd64/minishift start -v 5 --profile minishift --disk-size 40gb --memory 8092mb --openshift-version v3.10.0
-/root/minishift-1.23.0-linux-amd64/minishift delete -f
+/root/minishift-1.23.0-linux-amd64/minishift start -v 5 --profile minishift --disk-size 40gb --memory 8092mb --openshift-version v3.10.0 || /root/minishift-1.23.0-linux-amd64/minishift delete -f
