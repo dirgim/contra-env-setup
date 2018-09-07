49dfe07562f3913b3bf24edb162a3f3de84e8427
kpavic <kpavic@redhat.com>
2018-09-07 21:40:06 +0200

    Fix docker-machines network, temporary fix for minishift start

---
+++ test/config/Dockerfiles/contra-env-setup-test-f28/fix-minishift.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/root/minishift-1.23.0-linux-amd64/minishift start -v 5 --profile minishift --disk-size 40gb --memory 8092mb --openshift-version v3.10.0
+/root/minishift-1.23.0-linux-amd64/minishift delete -f
